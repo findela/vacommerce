417758db0a25b86886610bb0a70be9d827059e27
Tamaghna Banerjee <tamaghna.ba@imerit.net>
2022-06-06 22:11:37 +0530

    Updated Registration page UI

--- js/script.js
+++ js/script.js
@@ -1,1 +1,30 @@
 feather.replace();
+$(function() {
+  // city api list call
+  $("#stateList").attr("disabled", true);
+  $("#cityList").attr("disabled", true);
+  stateList();
+});
+
+cityList = () => {
+    $.get('https://demo7389246.mockable.io/cities/list', function(response){
+        if(response.length) {
+            response.forEach((city) => {
+                $("#cityList").append("<option>"+city.name+"</option>");
+            });
+            $("#cityList").attr("disabled", false);
+        }
+    });
+}
+
+stateList = () => {
+    $.get('https://demo7389246.mockable.io/state/list', function(response){
+        if(response.length) {
+            response.forEach((state) => {
+                $("#stateList").append("<option>"+state.name+"</option>");
+            });
+            $("#stateList").attr("disabled", false);
+            cityList();
+        }
+    });
+}
